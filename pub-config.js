b1f1e45fc948c3460ba0803aa3472557d7bd04d6
think2011 <452125301@qq.com>
2015-12-24 00:45:37 +0800

    Add publish.sh

--- pub-config.js
+++ pub-config.js
@@ -1,45 +1,45 @@
 var opts = module.exports = {
 
-  appUrl: 'http://think2011.net/lodash-zh',
-  github: 'https://github.com/think2011/lodash-zh',
-  version: '4.0.0-pre',
+    appUrl : 'http://think2011.net/lodash-zh',
+    github : 'https://github.com/think2011/lodash-zh',
+    version: '4.0.0-pre',
 
-  noRobots: true,            // please don't crawl me (yet)
-  linkNewWindow: true,
-  folderPages: true,
-  editor: false,
-  throttleReload: '1s',
+    noRobots      : false,            // please don't crawl me (yet)
+    linkNewWindow : true,
+    folderPages   : true,
+    editor        : false,
+    throttleReload: '1s',
 
-  pkgs: [
-    'pub-pkg-prism',
-    'pub-pkg-font-awesome',
-    'pub-pkg-seo'
-  ],
+    pkgs: [
+        'pub-pkg-prism',
+        'pub-pkg-font-awesome',
+        'pub-pkg-seo'
+    ],
 
-  sources: [
-    { path:'./pages', writable:true },
-    { path:'./lodash/lodash.js', format: 'JsDOC' },
-    { path:'./templates', compile:'handlebars' }
-  ],
+    sources: [
+        {path: './pages', writable: true},
+        {path: './lodash/lodash.js', format: 'JsDOC'},
+        {path: './templates', compile: 'handlebars'}
+    ],
 
-  staticPaths: [
-    { path:'./lodash/lodash.js', route:'/js', inject:true },
-    './static',
-    './CNAME',
-    '.gitignore',
-    '.nojekyll',
-    './README.md'
-  ],
+    staticPaths: [
+        {path: './lodash/lodash.js', route: '/js', inject: true},
+        './static',
+        './CNAME',
+        '.gitignore',
+        '.nojekyll',
+        './README.md'
+    ],
 
-  generatorPlugins: './plugins/generator-plugin.js',
+    generatorPlugins: './plugins/generator-plugin.js',
 
-  browserScripts: { path:'./doc.js', route:'/js', inject:true },
+    browserScripts: {path: './doc.js', route: '/js', inject: true},
 
-  injectCss: ['/css/doc.css'],
+    injectCss: ['/css/doc.css'],
 
-  outputs: {
-    path: './lodash-zh',
-    relPaths: true
-  }
+    outputs: {
+        path    : './lodash-zh',
+        relPaths: true
+    }
 
 };
